5beeef74272d066a308511d98db7defe159a7ebf
renanphilipe <renanphilipe@hotmail.com>
2022-09-12 18:28:02 -0300

    Criação de configuração do projeto
    Foi criado e configurado o projeto NWL eSports

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+function Button() {
+  return (
+  <button>
+    Send
+  </button>)
+}
+
+function App() {
+  
+  return (
+  
+  <div>
+    <Button />
+    <Button />
+    <Button />
+  </div>
+  )
+  
+}
+
+export default App
