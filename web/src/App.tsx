a46fe6bdb488136832b5c85a59f63f3622bc069a
renanphilipe <renanphilipe@hotmail.com>
2022-09-14 01:29:29 -0300

    Adições de imagens e layout
    Foi feita a adição de imagens de background e das imagens dos games, além da configurações de estilo e posicionamento do componentes web.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,37 @@
 import './styles/main.css';
 
+import logoImg from   './assets/logo-nlw-esports.svg';
+
+
 function App() {
   
   return (
   
-  <div className="w-8 h-8 bg-violet-500">
-  
+  <div className='max-w[1344px] max-auto flex flex-col items-center my-20'>
+    <img src={logoImg}/>
+    <h1 className='text-6xl text-white font-black'>
+      Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.</h1>
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <a href=''>
+          <img src='/game-1.png'/>
+        </a>
+        <a href=''>
+          <img src='/game-2.png'/>
+        </a>
+        <a href=''>
+          <img src='/game-3.png'/>
+        </a>
+        <a href=''>
+          <img src='/game-4.png'/>
+        </a>
+        <a href=''>
+          <img src='/game-5.png'/>
+        </a>
+        <a href=''>
+          <img src='/game-6.png'/>
+        </a>
+      </div>
+
   </div>
   )
   
