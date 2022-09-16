02a3471803308472aea34b7c7ded0aeb5d6f0dce
renanphilipe <renanphilipe@hotmail.com>
2022-09-16 13:31:13 -0300

    Ajuste de estrutura e adição de atributos nas divs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -67,14 +67,21 @@ function App() {
         </a>
       </div>
 
-      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6 rounded-lg'>
-          asdasdw
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-8 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'> Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className='py-3 px-4 text-white bg-violet-500  hover:bg-violet-600 rounded'>
+            Publicar anúncio
+          </button>
+
         </div>
       </div>
     </div>
   )
-
 }
 
 export default App
