3d15961b77551ac0d963f1f004f2d93949e48b5c
renanphilipe <renanphilipe@hotmail.com>
2022-09-16 12:40:41 -0300

    Atualização de hidentação
    Atualização de hidentação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,19 @@
 import './styles/main.css';
 
-import logoImg from   './assets/logo-nlw-esports.svg';
+import logoImg from './assets/logo-nlw-esports.svg';
 
 
 function App() {
-  
+
   return (
-  
-  <div className='max-w[1344px] max-auto flex flex-col items-center my-20'>
-    <img src={logoImg}/>
-    <h1 className='text-6xl text-white font-black'>
-      Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.</h1>
+
+    <div className='max-w[1344px] max-auto flex flex-col items-center my-20'>
+      <img src={logoImg} />
+      <h1 className='text-6xl text-white font-black'>
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.</h1>
       <div className='grid grid-cols-6 gap-6 mt-16'>
         <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-1.png' alt=''/>
+          <img src='/game-1.png' alt='' />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>League of Legends</strong>
@@ -21,8 +21,8 @@ function App() {
           </div>
         </a>
 
-       <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-2.png' alt=''/>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-2.png' alt='' />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>Dota</strong>
@@ -31,7 +31,7 @@ function App() {
         </a>
 
         <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-3.png' alt=''/>
+          <img src='/game-3.png' alt='' />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>Counter Strike</strong>
@@ -40,7 +40,7 @@ function App() {
         </a>
 
         <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-4.png' alt=''/>
+          <img src='/game-4.png' alt='' />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>Apex Legends</strong>
@@ -49,7 +49,7 @@ function App() {
         </a>
 
         <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-5.png' alt=''/>
+          <img src='/game-5.png' alt='' />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>Fortnite</strong>
@@ -58,7 +58,7 @@ function App() {
         </a>
 
         <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src='/game-6.png' alt=''/>
+          <img src='/game-6.png' alt='' />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>World of Warcaft</strong>
@@ -66,10 +66,14 @@ function App() {
           </div>
         </a>
       </div>
+      <div className='pt-1 bg-nlw-gradient slef-stretch'>
+        <div className='bg-[#2A2634] px-8 py-6 mt-8 rounded-lg'>
 
-  </div>
+        </div>
+      </div>
+    </div>
   )
-  
+
 }
 
 export default App
