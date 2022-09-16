154b337172109e4f7e4d5735e7e64f2be73556a5
renanphilipe <renanphilipe@hotmail.com>
2022-09-16 18:04:05 -0300

    Deleção dos componentes, propriedades e impotações
    Retirada dos componentes, propriedades e importações que haviam sido digitados para aprender conceitos

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,42 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
 
-      <Button title='Send 1'/>      
-      <Button title='Send 2'/>
-      <Button title='Send 3'/>
-      <Button title='Send 4'/>
-      
-      <Text>Hello Reactive App</Text>
-      
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button (props:ButtonProps) {
-    return ( 
-      <TouchableOpacity>
-        <Text>
-          {props.title}
-        </Text>
-      </TouchableOpacity>
-      )
-    }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
