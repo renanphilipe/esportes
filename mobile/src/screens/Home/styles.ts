23a056de4614c75e2b38f2b64697c895110ac72c
renanphilipe <renanphilipe@hotmail.com>
2022-09-17 15:52:18 -0300

    Implementação do componente de logo  nlw-esports

--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -1,5 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,    
+    alignItems: 'center',    
+  },
+  logo: {
+    width: 214,
+    height: 120,
+    marginTop: 74,
+    marginBottom: 48}
 });
