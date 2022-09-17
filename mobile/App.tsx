fab317ca4726250ccba0e3ff6eda31d1cc86eddc
renanphilipe <renanphilipe@hotmail.com>
2022-09-17 14:47:05 -0300

    Implementação da StatusBar
    Configuração da StatusBar para que a barra de estatus fique visível na execução do nosso app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,15 @@
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import { Background } from './src/components/Background';
 
 export default function App() {
   return (
     <Background>
-
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent 
+      />
     </Background>
   );
 }
