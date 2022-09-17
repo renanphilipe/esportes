5c5831ae7b2bf343882039fae8c200c00e6bbb5e
renanphilipe <renanphilipe@hotmail.com>
2022-09-17 15:18:19 -0300

    Implementação da tela de home e de loading
    Foi implementada a tela de home e caso as fontes não tenham sido carregadas, será exibida a tela de loading.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,17 @@
 import { StatusBar } from 'react-native';
 
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
+
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+
 import { Background } from './src/components/Background';
 
+
 export default function App() {
+  const [fontsLoaded] = useFonts({Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black}); 
+
+
   return (
     <Background>
       <StatusBar 
@@ -10,6 +19,8 @@ export default function App() {
         backgroundColor="transparent"
         translucent 
       />
+
+      {fontsLoaded ? <Home/> : <Loading/>}
     </Background>
   );
 }
