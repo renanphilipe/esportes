a84d38762fb617895bad0565118725542f518b06
renanphilipe <renanphilipe@hotmail.com>
2022-09-23 19:52:20 -0300

    Implementação dos componentes para consumir os GameCards do banco de dados

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,30 @@
-import { MagnifyingGlassPlus } from 'phosphor-react'
+import { useState, useEffect } from 'react';
+import { MagnifyingGlassPlus } from 'phosphor-react';
 import { CreateAdBanner } from './components/CreateAdBanner';
 import logoImg from './assets/logo-nlw-esports.svg';
 import './styles/main.css';
 import { GameBanner } from './components/GameBanner';
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
+
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data);
+      })
+  }, [])
 
   return (
 
@@ -15,18 +35,16 @@ function App() {
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
 
-        <GameBanner bannerUrl='/game-1.png' title='League of Legends' adsCount={4} />
-
-        <GameBanner bannerUrl='/game-2.png' title='Dota 2' adsCount={2} />
-
-        <GameBanner bannerUrl='/game-3.png' title='Counter Strike CS:GO' adsCount={1} />
-
-        <GameBanner bannerUrl='/game-4.png' title='Apex Legends' adsCount={0} />
-
-        <GameBanner bannerUrl='/game-5.png' title='Fortnite' adsCount={1} />
-
-        <GameBanner bannerUrl='/game-6.png' title='World of Warcraft' adsCount={1} />
-
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
 
       <CreateAdBanner />
